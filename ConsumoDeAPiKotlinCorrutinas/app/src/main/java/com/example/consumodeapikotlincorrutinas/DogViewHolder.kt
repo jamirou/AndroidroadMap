38e4d983d180203d2f4bba95e1f0d85dc6a1b60f
Jamirou <devjamiro@gmail.com>
2024-08-27 18:32:46 -0400

    Load images with Picasso in DogViewHolder

--- ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/DogViewHolder.kt
+++ ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/DogViewHolder.kt
@@ -3,10 +3,11 @@ package com.example.consumodeapikotlincorrutinas
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.consumodeapikotlincorrutinas.databinding.ItemDogBinding
+import com.squareup.picasso.Picasso
 
 class DogViewHolder(view: View):RecyclerView.ViewHolder(view) {
     private val binding = ItemDogBinding.bind(view)
     fun bind(image:String) {
-        binding.ivDogImages
+        Picasso.get().load(image).into(binding.ivDogImages)
     }
 }
