bbfb79615e84a21f7a9c223087588e41f2d9c171
Jamirou <devjamiro@gmail.com>
2024-08-27 16:35:49 -0400

    Set up ViewBinding in MainActivity in MainActivity1; configure layout inflation and binding

--- ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
+++ ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
@@ -1,20 +1,15 @@
 package com.example.consumodeapikotlincorrutinas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.consumodeapikotlincorrutinas.databinding.ActivityMain1Binding
 
 class MainActivity1 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMain1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
