2df3d34b3267c00ba1fb6f8597d6ee426623c1ce
Jamirou <devjamiro@gmail.com>
2024-08-27 18:30:09 -0400

    Bind ImageView in DogViewHolder using ViewBinding

--- ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/DogViewHolder.kt
+++ ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/DogViewHolder.kt
@@ -2,9 +2,11 @@ package com.example.consumodeapikotlincorrutinas
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.consumodeapikotlincorrutinas.databinding.ItemDogBinding
 
 class DogViewHolder(view: View):RecyclerView.ViewHolder(view) {
+    private val binding = ItemDogBinding.bind(view)
     fun bind(image:String) {
-
+        binding.ivDogImages
     }
 }
