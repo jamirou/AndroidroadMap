3405f86bf6a6944aa6d988414c4ef8306c78c9b5
Jamirou <devjamiro@gmail.com>
2024-08-27 17:59:39 -0400

    Implement search functionality with coroutine for API calls

--- ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
+++ ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
@@ -3,6 +3,9 @@ package com.example.consumodeapikotlincorrutinas
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.consumodeapikotlincorrutinas.databinding.ActivityMain1Binding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -20,6 +23,18 @@ class MainActivity1 : AppCompatActivity() {
             .baseUrl("https://dog.ceo/api/breed/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    private fun searchByName(query:String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val dogs = call.body()
+            if (call.isSuccessful) {
+                //show recyclerview
+            } else {
+                //show error
+            }
+        }
     }
 
 }
