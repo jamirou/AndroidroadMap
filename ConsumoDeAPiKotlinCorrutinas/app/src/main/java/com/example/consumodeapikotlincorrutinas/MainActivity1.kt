5fbe942a7c135e62f4074118757b01f367b0fd23
Jamirou <devjamiro@gmail.com>
2024-08-28 10:29:32 -0400

    Fix theme inheritance to use Theme.AppCompat for proper widget support

--- ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
+++ ConsumoDeAPiKotlinCorrutinas/app/src/main/java/com/example/consumodeapikotlincorrutinas/MainActivity1.kt
@@ -2,6 +2,7 @@ package com.example.consumodeapikotlincorrutinas
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
@@ -54,9 +55,15 @@ class MainActivity1 : AppCompatActivity(), OnQueryTextListener {
                 } else {
                     showError()
                 }
+                hideKeyboard()
             }
 
         }
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewRoot.windowToken, 0)
     }
 
     private fun showError() {
