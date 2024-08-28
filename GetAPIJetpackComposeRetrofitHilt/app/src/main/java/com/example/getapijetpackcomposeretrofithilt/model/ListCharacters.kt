56f919fb6cc1156343916892116005d53919becf
Jamirou <devjamiro@gmail.com>
2024-08-28 14:40:17 -0400

    Generated data models for API response using plugin

---
+++ GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/model/ListCharacters.kt
@@ -0,0 +1,6 @@
+package com.example.getapijetpackcomposeretrofithilt.model
+
+data class ListCharacters(
+    val info: Info,
+    val results: List<Result>
+)
---
+++ GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/model/Location.kt
@@ -0,0 +1,6 @@
+package com.example.getapijetpackcomposeretrofithilt.model
+
+data class Location(
+    val name: String,
+    val url: String
+)
