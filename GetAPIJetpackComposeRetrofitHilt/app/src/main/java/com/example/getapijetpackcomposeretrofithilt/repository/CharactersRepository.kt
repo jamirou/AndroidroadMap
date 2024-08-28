ac3283b4e9e22bc150b9965d7378af971bff160f
Jamirou <devjamiro@gmail.com>
2024-08-28 14:46:09 -0400

    Implemented CharactersRepository to handle data fetching from API

--- GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/repository/CharactersRepository.kt
+++ GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/repository/CharactersRepository.kt
@@ -1,4 +1,13 @@
 package com.example.getapijetpackcomposeretrofithilt.repository
 
-class CharactersRepository {
+import com.example.getapijetpackcomposeretrofithilt.api.CharactersAPI
+import com.example.getapijetpackcomposeretrofithilt.model.ListCharacters
+import javax.inject.Inject
+
+class CharactersRepository @Inject constructor(
+    private val charactersAPI: CharactersAPI
+) {
+    suspend fun getCharacters() : ListCharacters {
+        return charactersAPI.getCharacters()
+    }
 }
