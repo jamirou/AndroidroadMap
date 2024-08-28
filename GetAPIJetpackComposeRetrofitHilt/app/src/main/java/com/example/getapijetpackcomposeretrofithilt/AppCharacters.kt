d64c7d84d767f84bcd903bca7eb856c81073a187
Jamirou <devjamiro@gmail.com>
2024-08-28 14:26:33 -0400

    Set up Hilt application class and registered in manifest

---
+++ GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/AppCharacters.kt
@@ -0,0 +1,8 @@
+package com.example.getapijetpackcomposeretrofithilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppCharacters: Application() {
+}
