b1eeffee4821e0b5da75f5c5e2a7883857a89386
Jamirou <devjamiro@gmail.com>
2024-08-29 19:01:15 -0400

    Added Chain for constraint

--- ExercisingJetpackCompose/app/src/main/java/com/example/exercisingjetpackcompose/ConstraintLayoutExample2.kt
+++ ExercisingJetpackCompose/app/src/main/java/com/example/exercisingjetpackcompose/ConstraintLayoutExample2.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 
 //@Preview(showSystemUi = false)
@@ -32,7 +32,7 @@ fun ConstraintLayoutGuide() {
     }
 }
 
-@Preview()
+//@Preview()
 @Composable
 fun ConstraintBarrier() {
     ConstraintLayout(modifier = Modifier.fillMaxSize()) {
@@ -58,6 +58,47 @@ fun ConstraintBarrier() {
             .constrainAs(boxYellow) {
 //                top.linkTo(boxGreen.bottom)
             }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintChain() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        val (boxRed, boxBlack, boxWhite) = createRefs()
+        createHorizontalChain(
+            boxRed, boxBlack, boxWhite,
+            chainStyle = ChainStyle.Packed
+        )
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                start.linkTo(parent.start)
+                end.linkTo(boxBlack.start)
+            }
+
+        )
+        Box(modifier = Modifier
+            .size(115.dp)
+            .background(Color.Black)
+            .constrainAs(boxBlack) {
+                start.linkTo(boxRed.end)
+                end.linkTo(boxWhite.start)
+            }
+
+        )
+        Box(modifier = Modifier
+            .size(105.dp)
+            .background(Color.White)
+            .constrainAs(boxWhite) {
+                start.linkTo(boxBlack.end)
+                end.linkTo(parent.end)
+            }
+
         )
     }
 }
