e64c04020005fb4063f1c86cabf4044a99d2b832
Jamirou <devjamiro@gmail.com>
2024-09-03 11:58:31 -0400

    Adding new function on ui

--- GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/screens/CharactersScreen.kt
+++ GetAPIJetpackComposeRetrofitHilt/app/src/main/java/com/example/getapijetpackcomposeretrofithilt/screens/CharactersScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
