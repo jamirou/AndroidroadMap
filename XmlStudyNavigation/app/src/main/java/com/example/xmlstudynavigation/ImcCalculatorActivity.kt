6ae78361fe751b608531d9fa82256fbe9f6971b0
Jamirou <devjamiro@gmail.com>
2024-09-03 22:45:59 -0400

    feat(logic): implemented logic for male and female buttons for ImcCalculator

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/ImcCalculatorActivity.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/ImcCalculatorActivity.kt
@@ -3,10 +3,18 @@ package com.example.xmlstudynavigation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var selectedGender: Gender = Gender.MALE
+    private lateinit var cardMale: CardView
+    private lateinit var cardFemale: CardView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,46 @@ class ImcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initComponents()
+        initListeners()
+        initUI()
     }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
+    private fun initListeners() {
+        cardMale.setOnClickListener {
+            changeGender(Gender.MALE)
+        }
+        cardFemale.setOnClickListener {
+            changeGender(Gender.FEMALE)
+        }
+    }
+
+    private fun initComponents() {
+        cardMale = findViewById(R.id.CardMale)
+        cardFemale = findViewById(R.id.CardFemale)
+    }
+
+    private fun changeGender(gender: Gender) {
+        selectedGender = gender
+        setGenderColor()
+    }
+
+    private fun setGenderColor() {
+        cardMale.setCardBackgroundColor(setBackgroundColor(selectedGender == Gender.MALE))
+        cardFemale.setCardBackgroundColor(setBackgroundColor(selectedGender == Gender.FEMALE))
+    }
+
+    private fun setBackgroundColor(isSelectedComponent: Boolean): Int {
+        val currentCardColor = if (isSelectedComponent) {
+            R.color.pressed_color
+        } else {
+            R.color.normal_color
+        }
+        return ContextCompat.getColor(this, currentCardColor)
+    }
+
 }
