6cfb52a5d0f44c0b01be6b7143d1c2e8d4b75f3a
Jamirou <devjamiro@gmail.com>
2024-09-05 13:02:33 -0400

    feat(tasks): Task model and TaskViewHolder for displaying task

---
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/view/adapter/TasksViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.xmlstudynavigation.view.adapter
+
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.xmlstudynavigation.R
+import com.example.xmlstudynavigation.model.Task
+
+class TasksViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private val textViewTasks:TextView = view.findViewById(R.id.TextViewTasks)
+    private val checkBox:CheckBox = view.findViewById(R.id.CheckBoxTasks)
+
+    fun render(task: Task) {
+        textViewTasks.text = task.name
+    }
+
+}
