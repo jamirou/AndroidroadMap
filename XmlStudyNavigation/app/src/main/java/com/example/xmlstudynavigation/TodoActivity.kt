cc0398b1ae7eaa510245f0f3015c3e0619d0a61f
Jamirou <devjamiro@gmail.com>
2024-09-04 23:23:07 -0400

    feat(todo): create TodoActivity and setup initial layout and resources Created the todoActivity along with its layout. Updated colors and strings resources for the Todo app.

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/TodoActivity.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/TodoActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class TodoActivity : AppCompatActivity() {
+
+    private lateinit var recyclerviewCategory: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,15 @@ class TodoActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initComponents()
+        initUI()
+    }
+
+    private fun initUI() {
+
+    }
+
+    private fun initComponents() {
+        recyclerviewCategory = findViewById(R.id.RecyclerViewCategories)
     }
 }
