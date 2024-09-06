b6f690756d829dca92466cfd7fb0c5e80f9a0ad5
Jamirou <devjamiro@gmail.com>
2024-09-06 17:38:28 -0400

    feat(categories): implement selection logic for TaskCategory in adapter and viewholder

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/view/adapter/CategoriesAdapter.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/view/adapter/CategoriesAdapter.kt
@@ -6,16 +6,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.xmlstudynavigation.R
 import com.example.xmlstudynavigation.model.TaskCategory
 
-class CategoriesAdapter(private val categories:List<TaskCategory>):RecyclerView.Adapter<CategoriesViewHolder>() {
+class CategoriesAdapter(
+    private val categories: List<TaskCategory>,
+    private val onItemSelected: (Int) -> Unit
+) :
+    RecyclerView.Adapter<CategoriesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoriesViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_tasks_categories, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_tasks_categories, parent, false)
         return CategoriesViewHolder(view)
     }
 
     override fun getItemCount() = categories.size
 
     override fun onBindViewHolder(holder: CategoriesViewHolder, position: Int) {
-        holder.render(categories[position])
+        holder.render(categories[position], onItemSelected)
     }
 }
--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/view/adapter/CategoriesViewHolder.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/view/adapter/CategoriesViewHolder.kt
@@ -2,6 +2,8 @@ package com.example.xmlstudynavigation.view.adapter
 
 import android.view.View
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.xmlstudynavigation.R
 import com.example.xmlstudynavigation.model.TaskCategory
@@ -9,8 +11,24 @@ import com.example.xmlstudynavigation.model.TaskCategory
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val tvItemCategoryName: TextView = view.findViewById(R.id.TvItemCategoryName)
     private val viewDividerItemTasks: View = view.findViewById(R.id.view_divider_item_tasks)
+    private val cardViewContainer: CardView = view.findViewById(R.id.cardViewContainer)
 
-    fun render(taskCategory: TaskCategory) {
+    fun render(taskCategory: TaskCategory, onItemSelected: (Int) -> Unit) {
+
+        val colorSelected = if (taskCategory.isSelected) {
+            R.color.pressed_color
+        } else {
+            R.color.normal_color
+        }
+
+        cardViewContainer.setCardBackgroundColor(
+            ContextCompat.getColor(
+                cardViewContainer.context,
+                colorSelected
+            )
+        )
+
+        itemView.setOnClickListener { onItemSelected(layoutPosition) }
 
         when (taskCategory) {
             TaskCategory.Exercise -> {
