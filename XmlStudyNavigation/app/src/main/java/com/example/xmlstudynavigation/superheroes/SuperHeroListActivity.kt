5d7523c7f8e08cc2a91e863051347c7a38045b15
Jamiro'sMac <apple@Jamiros-Mac.local>
2024-09-09 20:56:32 -0300

    feat(superHeroLitActivity): initialize ViewBinding for layout inflation

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.xmlstudynavigation.R
+import com.example.xmlstudynavigation.databinding.ActivitySuperHeroListBinding
 
 class SuperHeroListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySuperHeroListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_super_hero_list)
+        binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
