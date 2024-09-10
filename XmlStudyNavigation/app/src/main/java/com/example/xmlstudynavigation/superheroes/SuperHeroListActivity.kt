a87aa5c114a5a9a884b4b77b19bb2de09d3517fe
Jamiro <devjamiro@gmail.com>
2024-09-09 23:33:11 -0300

    feat(SuperHeroListActivity): configure Retrofit for API calls

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
@@ -3,24 +3,54 @@ package com.example.xmlstudynavigation.superheroes
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.xmlstudynavigation.R
 import com.example.xmlstudynavigation.databinding.ActivitySuperHeroListBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuperHeroListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySuperHeroListBinding
+    private lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivitySuperHeroListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        retrofit = getRetrofit()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initUI()
     }
+
+    private fun initUI() {
+        binding.SearchViewSearchHero.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByNam(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?) = false
+        })
+    }
+
+    private fun searchByNam(query: String) {
+
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder().baseUrl("https://superheroapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
 }
