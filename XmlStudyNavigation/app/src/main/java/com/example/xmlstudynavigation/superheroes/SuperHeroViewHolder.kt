0f6872a6682a783bb193784581a6c3cc383156ca
Jamiro <devjamiro@gmail.com>
2024-09-10 18:30:36 -0300

    feat(SuperHeroViewHolder): create view holder for binding super hero data

---
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.xmlstudynavigation.superheroes
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.xmlstudynavigation.model.SuperHeroesData
+
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(superHero: SuperHeroesData) {
+
+    }
+
+}
