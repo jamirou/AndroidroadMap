e8dc79a95085a5f1979c80ff645ad9fad0a2ea62
Jamiro <devjamiro@gmail.com>
2024-09-10 15:28:49 -0300

    feat(SuperHeroModel): add model handling SupeHero API response"

---
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/model/SuperHeroModel.kt
@@ -0,0 +1,5 @@
+package com.example.xmlstudynavigation.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SuperHeroModel(@SerializedName("response") val responseStatus: String)
