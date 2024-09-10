0835878b2cbe1dddcb5841cad677c96cb5702d82
Jamiro <devjamiro@gmail.com>
2024-09-10 18:27:56 -0300

    feat(SuperHeroModel): update model to include super hero details

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/model/SuperHeroModel.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/model/SuperHeroModel.kt
@@ -2,4 +2,23 @@ package com.example.xmlstudynavigation.model
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperHeroModel(@SerializedName("response") val responseStatus: String)
+data class SuperHeroModel(
+    @SerializedName("response") val responseStatus: String,
+    @SerializedName("results") val superHeroes: List<SuperHeroesData>
+)
+
+data class SuperHeroesData(
+    @SerializedName("id") val superHeroId: String,
+    @SerializedName("name") val superHeroName: String,
+//    @SerializedName("powerstats") val superHeroPowerStats: List<SuperHeroesStats>
+)
+
+data class SuperHeroesStats(
+    @SerializedName("intelligence") val superHeroIntelligence: String,
+    @SerializedName("strength") val superHeroStrength: String,
+    @SerializedName("speed") val superHeroSpeed: String,
+    @SerializedName("durability") val superHeroDurability: String,
+    @SerializedName("power") val superHeroPower: String,
+    @SerializedName("combat") val superHeroCombat: String
+)
+
