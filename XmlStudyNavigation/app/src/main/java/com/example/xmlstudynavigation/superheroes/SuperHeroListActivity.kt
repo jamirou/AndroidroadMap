79dc627d6c04e42611e0b4f1dee67f5fb46e3670
Jamiro <devjamiro@gmail.com>
2024-09-10 15:27:12 -0300

    feat(SuperHeroListActivity): implement search functionality with API call using Retrofit and coroutines

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroListActivity.kt
@@ -1,6 +1,7 @@
 package com.example.xmlstudynavigation.superheroes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
@@ -8,6 +9,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.xmlstudynavigation.R
 import com.example.xmlstudynavigation.databinding.ActivitySuperHeroListBinding
+import com.example.xmlstudynavigation.model.SuperHeroModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -34,7 +40,7 @@ class SuperHeroListActivity : AppCompatActivity() {
         binding.SearchViewSearchHero.setOnQueryTextListener(object :
             SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                searchByNam(query.orEmpty())
+                searchByName(query.orEmpty())
                 return false
             }
 
@@ -42,15 +48,22 @@ class SuperHeroListActivity : AppCompatActivity() {
         })
     }
 
-    private fun searchByNam(query: String) {
-
-    }
-
     private fun getRetrofit(): Retrofit {
-        return Retrofit
-            .Builder().baseUrl("https://superheroapi.com/api/")
+        return Retrofit.Builder().baseUrl("https://superheroapi.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    private fun searchByName(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val responseStatus: Response<SuperHeroModel> =
+                retrofit.create(ApiService::class.java).getSuperHeroes(query)
+            if (responseStatus.isSuccessful) {
+                Log.i("ResponseStatus", "${responseStatus.body()}")
+            } else {
+                Log.i("ResponseStatus", "Error")
+            }
+        }
     }
 
 }
