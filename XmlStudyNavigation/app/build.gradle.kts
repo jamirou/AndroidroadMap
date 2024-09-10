22fa8106bd6984d6b007b32f1a4b75318b9772b3
Jamiro <devjamiro@gmail.com>
2024-09-09 23:32:35 -0300

    feat(gradle): add Retrofit and Gson converter dependencies

--- XmlStudyNavigation/app/build.gradle.kts
+++ XmlStudyNavigation/app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
