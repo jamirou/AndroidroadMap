2d7406881bfccd0e2c7776b79c473490ed762a18
Jamiro <devjamiro@gmail.com>
2024-09-11 18:46:12 -0300

    feat(ApiService): add endpoint for fetching detailed suoerhero information

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/ApiService.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/ApiService.kt
@@ -1,14 +1,19 @@
 package com.example.xmlstudynavigation.superheroes
 
+import com.example.xmlstudynavigation.model.SuperHeroItemResponse
 import com.example.xmlstudynavigation.model.SuperHeroModel
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-    @GET("api/0e12efb2ddeab767485c2f4e1b20cc89/search/{name}")
+    @GET("/api/0e12efb2ddeab767485c2f4e1b20cc89/search/{name}")
     suspend fun getSuperHeroes(
         @Path("name") superHeroName: String
     ): Response<SuperHeroModel>
+
+    @GET("/api/0e12efb2ddeab767485c2f4e1b20cc89/{character_id}")
+    suspend fun getSuperHeroDetail(@Path("character_id") superHeroId: String): Response<SuperHeroItemResponse>
+
 }
 
