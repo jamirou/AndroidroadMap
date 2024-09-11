b23d4b3a0792fe7ba1c5c4afd21d0245ed2fc093
Jamiro <devjamiro@gmail.com>
2024-09-10 23:47:56 -0300

    feat(SuperHeroAdapter): add function to update super hero list

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroAdapter.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroAdapter.kt
@@ -10,6 +10,11 @@ class SuperHeroAdapter(
     var superHeroList: List<SuperHeroesData> = emptyList()
 ) : RecyclerView.Adapter<SuperHeroViewHolder>() {
 
+    fun updateSuperHeroList(superHeroList: List<SuperHeroesData>) {
+        this.superHeroList = superHeroList
+        notifyDataSetChanged()
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
         return SuperHeroViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
