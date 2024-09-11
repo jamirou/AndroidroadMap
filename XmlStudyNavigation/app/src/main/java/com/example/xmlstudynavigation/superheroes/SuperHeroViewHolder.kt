c01f57d107151995f7ec0db8ec44ce95beeab44d
Jamiro <devjamiro@gmail.com>
2024-09-11 10:12:33 -0300

    feat(superHeroViewHolder): display super hero image using Picasso

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroViewHolder.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroViewHolder.kt
@@ -4,12 +4,17 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.xmlstudynavigation.databinding.ItemSuperheroBinding
 import com.example.xmlstudynavigation.model.SuperHeroesData
+import com.squareup.picasso.Picasso
 
 class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemSuperheroBinding.bind(view)
     fun bind(superHeroItemResponse: SuperHeroesData) {
         binding.TextViewSuperHeroName.text = superHeroItemResponse.superHeroName
+
+        Picasso.get().load(superHeroItemResponse.superHeroImage.superHeroImageUrl)
+            .into(binding.ImageViewSuperHero)
     }
+
 
 }
