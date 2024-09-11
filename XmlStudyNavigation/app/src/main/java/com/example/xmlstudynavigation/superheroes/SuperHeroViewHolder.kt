d424052feec75a836c304ea5a901a1b9a83d68b9
Jamiro <devjamiro@gmail.com>
2024-09-10 23:49:19 -0300

    feat(SuperHeroViewHolder): bind super hero name in ViewHolder

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroViewHolder.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/superheroes/SuperHeroViewHolder.kt
@@ -2,12 +2,14 @@ package com.example.xmlstudynavigation.superheroes
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.xmlstudynavigation.databinding.ItemSuperheroBinding
 import com.example.xmlstudynavigation.model.SuperHeroesData
 
 class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind(superHero: SuperHeroesData) {
-
+    private val binding = ItemSuperheroBinding.bind(view)
+    fun bind(superHeroItemResponse: SuperHeroesData) {
+        binding.TextViewSuperHeroName.text = superHeroItemResponse.superHeroName
     }
 
 }
