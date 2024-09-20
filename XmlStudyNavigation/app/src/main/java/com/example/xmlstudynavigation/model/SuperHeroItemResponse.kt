ac297383cce7b8b67652f66f53ee659ab69b9de4
Jamiro <devjamiro@gmail.com>
2024-09-20 18:05:02 -0300

    Update: SuperHeroItemResponse model to parse aliases as a list

--- XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/model/SuperHeroItemResponse.kt
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/model/SuperHeroItemResponse.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class SuperHeroItemResponse(
     @SerializedName("name") val superHeroName: String,
     @SerializedName("powerstats") val powerStats: SuperHeroPowerStats,
-    @SerializedName("image") val superHeroImage: SuperHeroImageDetailResponse
+    @SerializedName("image") val superHeroImage: SuperHeroImageDetailResponse,
+    @SerializedName("biography") val superHeroBiography: SuperHeroBiography
 )
 
 data class SuperHeroPowerStats(
@@ -19,4 +20,13 @@ data class SuperHeroPowerStats(
 
 data class SuperHeroImageDetailResponse(
     @SerializedName("url") val superHeroImageUrl: String
+)
+
+data class SuperHeroBiography(
+    @SerializedName("full-name") val superHeroFullName: String,
+    @SerializedName("alter-egos") val superHeroAlterEgos: String,
+    @SerializedName("aliases") val superHeroAliases: List<String>,
+    @SerializedName("first-appearance") val superHeroFirstAppearance: String,
+    @SerializedName("publisher") val superHeroPublisher: String,
+    @SerializedName("alignment") val superHeroAlignment: String
 )
