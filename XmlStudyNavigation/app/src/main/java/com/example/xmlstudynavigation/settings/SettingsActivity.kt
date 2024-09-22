66f5c619f15a392fe8c54059e171edb6d3518bd6
Jamiro <devjamiro@gmail.com>
2024-09-22 17:15:24 -0300

    Add settingsActivity with ViewBinding setup for navigation

---
+++ XmlStudyNavigation/app/src/main/java/com/example/xmlstudynavigation/settings/SettingsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.xmlstudynavigation.settings
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.xmlstudynavigation.R
+import com.example.xmlstudynavigation.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
