b24053b3d6f25f8db95b886cc2864b4c6ecac924
Jamiro <devjamiro@gmail.com>
2024-09-23 19:54:02 -0300

    Added DataStore dependency for storing settings preferences

--- XmlStudyNavigation/app/build.gradle.kts
+++ XmlStudyNavigation/app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
 
+    //DataStore
+    implementation(libs.androidx.datastore.preferences)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
