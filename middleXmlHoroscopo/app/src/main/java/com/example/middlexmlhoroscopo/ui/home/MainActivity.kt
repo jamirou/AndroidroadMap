521feebb86c9918af4e157ea99cb7569a18fdbe8
Jamiro <devjamiro@gmail.com>
2024-09-29 18:53:06 -0300

    feat: implement navigation with BottomNavigationView in MainActivity

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/home/MainActivity.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/home/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.middlexmlhoroscopo.R
 import com.example.middlexmlhoroscopo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.FragmentContainerViewId) as NavHostFragment
+        navigationController = navHost.navController
+        binding.BottomNavigationView.setupWithNavController(navigationController)
     }
 }
