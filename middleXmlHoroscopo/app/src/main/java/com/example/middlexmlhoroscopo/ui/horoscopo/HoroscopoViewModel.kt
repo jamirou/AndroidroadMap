9095fe8b9c43420333dc11aa2e474823f2b79af8
Jamiro <devjamiro@gmail.com>
2024-09-30 16:59:48 -0300

    feat: add ViewModels for Horoscopo and Luck fragments

---
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.middlexmlhoroscopo.ui.horoscopo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HoroscopoViewModel @Inject constructor() : ViewModel() {
+}
