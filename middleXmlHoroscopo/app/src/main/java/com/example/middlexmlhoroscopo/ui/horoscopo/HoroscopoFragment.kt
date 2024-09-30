3739920d674297cca0a25873c9ecbbb5137a0293
Jamiro <devjamiro@gmail.com>
2024-09-30 16:59:21 -0300

    feat: set up Dagger Hilt for dependency injection across the app

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.middlexmlhoroscopo.databinding.FragmentHoroscopoBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HoroscopoFragment : Fragment() {
+
+    private val horoscopoViewModel by viewModels<HoroscopoViewModel>()
+
     private var _binding: FragmentHoroscopoBinding? = null
     private val binding get() = _binding!!
 
