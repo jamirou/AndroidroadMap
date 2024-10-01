1b5bd416927a94b364fefe9bb75eb239fef43096
Jamiro <devjamiro@gmail.com>
2024-09-30 23:07:17 -0300

    feat: connect HoroscopoFragment with ViewModel using stateFlow and set up RecyclerView

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
@@ -1,13 +1,18 @@
 package com.example.middlexmlhoroscopo.ui.horoscopo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.middlexmlhoroscopo.databinding.FragmentHoroscopoBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopoFragment : Fragment() {
@@ -16,6 +21,26 @@ class HoroscopoFragment : Fragment() {
 
     private var _binding: FragmentHoroscopoBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+
+    }
+
+    private fun initUI() {
+        initUiState()
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopoViewModel.horoscopo.collect {
+                    Log.i("Horoscopo", "initUI: $it")
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
