b2e61479f5910331b566c61722eda900e044ca44
Jamiro <devjamiro@gmail.com>
2024-10-01 19:27:22 -0300

    feat: add HroscopoAdapter, ViewHolder, and item layout for RecyclerView

---
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/adapter/HoroscopoViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.middlexmlhoroscopo.ui.horoscopo.adapter
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.middlexmlhoroscopo.databinding.ItemHoroscopoBinding
+import com.example.middlexmlhoroscopo.domain.model.HoroscopoInfo
+
+class HoroscopoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemHoroscopoBinding.bind(view)
+
+    fun render(horoscopoInfo: HoroscopoInfo) {
+        val context: Context = binding.root.context
+        binding.ImageViewHoroscopo.setImageResource(horoscopoInfo.img)
+        binding.TextViewHoroscopo.text = context.getString(horoscopoInfo.name)
+    }
+}
+
