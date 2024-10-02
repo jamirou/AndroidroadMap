487e5f084ba9ae04c6b9f7eb87f9942ac2d8bc60
Jamiro <devjamiro@gmail.com>
2024-10-02 17:28:28 -0300

    feat: add rotation animation with lambda callback in HoroscopoViewHolder

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/adapter/HoroscopoViewHolder.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/adapter/HoroscopoViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.middlexmlhoroscopo.ui.horoscopo.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.middlexmlhoroscopo.databinding.ItemHoroscopoBinding
 import com.example.middlexmlhoroscopo.domain.model.HoroscopoInfo
@@ -14,7 +15,20 @@ class HoroscopoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val context: Context = binding.root.context
         binding.ImageViewHoroscopo.setImageResource(horoscopoInfo.img)
         binding.TextViewHoroscopo.text = context.getString(horoscopoInfo.name)
-        binding.Parent.setOnClickListener { onItemSelected(horoscopoInfo) }
+        binding.Parent.setOnClickListener {
+            startRotation(binding.ImageViewHoroscopo, newLambda = { onItemSelected(horoscopoInfo) })
+            /*onItemSelected(horoscopoInfo)*/
+        }
+    }
+
+    private fun startRotation(view: View, newLambda: () -> Unit) {
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            withEndAction { newLambda() }
+            start()
+        }
     }
 }
 
