7276c23d88f4dcb86470377ceac16e9882d3d5a3
Jamiro <devjamiro@gmail.com>
2024-10-03 12:45:06 -0300

    refactor: navigate to detail using SafeArgs in HoroscopoFragment

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.middlexmlhoroscopo.databinding.FragmentHoroscopoBinding
 import com.example.middlexmlhoroscopo.ui.horoscopo.adapter.HoroscopoAdapter
@@ -38,11 +38,9 @@ class HoroscopoFragment : Fragment() {
 
     private fun initList() {
         horoscopoAdapter = HoroscopoAdapter(onItemSelected = {
-            Toast.makeText(
-                context,
-                getString(it.name),
-                Toast.LENGTH_LONG
-            ).show()
+            findNavController().navigate(
+                HoroscopoFragmentDirections.actionHoroscopoFragmentToHoroscopoDetailActivity()
+            )
         })
 
         binding.RecyclerViewHoroscopo.apply {
