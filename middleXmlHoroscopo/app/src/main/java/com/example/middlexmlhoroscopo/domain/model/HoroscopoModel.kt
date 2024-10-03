d39e8c82a8e0bacdd1c9e2ccb6cc77b9fb40c785
Jamiro <devjamiro@gmail.com>
2024-10-03 14:05:58 -0300

    feat: create HoroscopoModel to extend horoscopo properties

---
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/domain/model/HoroscopoModel.kt
@@ -0,0 +1,16 @@
+package com.example.middlexmlhoroscopo.domain.model
+
+enum class HoroscopoModel {
+    Aries,
+    Taurus,
+    Geminis,
+    Cancer,
+    Leo,
+    Virgo,
+    Libra,
+    Scorpio,
+    Sagittarius,
+    Capricorn,
+    Aquarius,
+    Pisces
+}
