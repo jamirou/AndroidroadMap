9afd5cae8648baac961ff02055ebf55a98808a13
Jamiro <devjamiro@gmail.com>
2024-10-03 12:45:36 -0300

    feat: add HoroscopoDetailActivity and implement SafeArgs navigation

---
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/detail/HoroscopoDetailActivity.kt
@@ -0,0 +1,33 @@
+
+package com.example.middlexmlhoroscopo.ui.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.middlexmlhoroscopo.R
+import com.example.middlexmlhoroscopo.databinding.ActivityHoroscopoDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopoDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHoroscopoDetailBinding
+    private val horoscopoDetailViewModel: HoroscopoDetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHoroscopoDetailBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/detail/HoroscopoDetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.middlexmlhoroscopo.ui.detail
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HoroscopoDetailViewModel @Inject constructor(): ViewModel() {
+}
