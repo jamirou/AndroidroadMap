d24fd678b6d547a454b14f08000b16f3fcaea4b8
Jamiro <devjamiro@gmail.com>
2024-10-07 18:19:17 -0300

    feat: add HoroscopoApiService interface for fetching horoscope data

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/data/network/HoroscopoApiService.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/data/network/HoroscopoApiService.kt
@@ -1,9 +1,10 @@
 package com.example.middlexmlhoroscopo.data.network
 
+import com.example.middlexmlhoroscopo.data.network.response.PredictionResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface HoroscopoApiService {
     @GET ("/{sign}")
-    suspend fun getHoroscopo(@Path("sign") sign:String)
+    suspend fun getHoroscopo(@Path("sign") sign:String): PredictionResponse
 }
