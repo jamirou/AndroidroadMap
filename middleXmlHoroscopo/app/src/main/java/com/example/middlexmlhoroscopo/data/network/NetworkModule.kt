36c46389c07ce896a7834ce9df2839146a84f10a
Jamiro <devjamiro@gmail.com>
2024-10-10 15:02:37 -0300

    feat: configure OkHttpClient with HttpLoggingInterceptor for Http request logging

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/data/network/NetworkModule.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/data/network/NetworkModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,11 +17,23 @@ import javax.inject.Singleton
 object NetworkModule {
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit
             .Builder()
             .baseUrl("https://newastro.vercel.app/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkkHttpClient(): OkHttpClient {
+
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
             .build()
     }
 
