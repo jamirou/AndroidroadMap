f06a9eccb469adec2d053e8daff830e6325a3313
Jamiro <devjamiro@gmail.com>
2024-10-14 12:47:45 -0300

    Enhance LuckFragment with card animation logic roulette event handling

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/luck/LuckFragment.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/luck/LuckFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import com.example.middlexmlhoroscopo.R
 import com.example.middlexmlhoroscopo.databinding.ActivityMainBinding
 import com.example.middlexmlhoroscopo.databinding.FragmentLuckBinding
@@ -40,7 +44,23 @@ class LuckFragment : Fragment() {
             ObjectAnimator.ofFloat(binding.ImageViewRoulette, View.ROTATION, 0f, degrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
         animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                binding.ImageViewReverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {}
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+        binding.ImageViewReverse.startAnimation(slideUpAnimation)
     }
 
     override fun onCreateView(
