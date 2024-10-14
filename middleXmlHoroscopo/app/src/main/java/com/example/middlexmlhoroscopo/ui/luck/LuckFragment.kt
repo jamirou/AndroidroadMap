db1e4458d43ecf618987e7559e70b55cf0d22f96
Jamiro <devjamiro@gmail.com>
2024-10-14 12:04:19 -0300

    feat: add roulette spin animation in LuckFragment - Implement ObjectAnimator to animate the roulette image on click - Add click listener to trigger roulette spin -Use DecelerateInterpolator to smooth out rotation - Replace the FragmentLayout with ConstraintLayout for better layout handling

--- middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/luck/LuckFragment.kt
+++ middleXmlHoroscopo/app/src/main/java/com/example/middlexmlhoroscopo/ui/luck/LuckFragment.kt
@@ -1,20 +1,47 @@
 package com.example.middlexmlhoroscopo.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import com.example.middlexmlhoroscopo.R
 import com.example.middlexmlhoroscopo.databinding.ActivityMainBinding
 import com.example.middlexmlhoroscopo.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ImageViewRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette() {
+        val random = Random()
+        val degrees = random.nextInt(1440) + 360
+
+        val animator =
+            ObjectAnimator.ofFloat(binding.ImageViewRoulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.start()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
