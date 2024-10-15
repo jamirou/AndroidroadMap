d6930e3a809a2f5f3d0a9345150f8f06c3504453
Jamiro <devjamiro@gmail.com>
2024-10-15 12:52:02 -0300

    feat(dependencies): add CamerX dependencies for camera functionality

--- middleXmlHoroscopo/app/build.gradle.kts
+++ middleXmlHoroscopo/app/build.gradle.kts
@@ -67,6 +67,14 @@ dependencies {
     /*Interceptor*/
     implementation(libs.logging.interceptor)
 
+    /*CameraX*/
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
+
 
 
     implementation(libs.androidx.core.ktx)
