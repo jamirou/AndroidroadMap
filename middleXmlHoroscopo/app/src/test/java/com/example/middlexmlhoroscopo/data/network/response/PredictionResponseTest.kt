de4cbb265b62263cdf438c5887a273e7c28efef4
Jamiro <devjamiro@gmail.com>
2024-10-15 21:35:05 -0300

    test(predictionresponse): add unit test for toDomain method in PredictionResponse

--- middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/data/network/response/PredictionResponseTest.kt
+++ middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/data/network/response/PredictionResponseTest.kt
@@ -1,5 +1,19 @@
 package com.example.middlexmlhoroscopo.data.network.response
 
-import org.junit.Assert.*
+import com.example.middlexmlhoroscopo.motherobject.HoroscopoMotherObject
+import io.kotlintest.shouldBe
+import org.junit.Test
 
-class PredictionResponseTest
+class PredictionResponseTest {
+    @Test
+    fun `toDomain should return a correct PredictionModel`() {
+        /*Given*/
+        val horoscopoResponse = HoroscopoMotherObject.anyResponse
+        /*When*/
+        val predictionModel = horoscopoResponse.toDomain()
+        /*Then*/
+        predictionModel.sign shouldBe  horoscopoResponse.sign
+        predictionModel.horoscopo shouldBe horoscopoResponse.horoscopo
+
+    }
+}
