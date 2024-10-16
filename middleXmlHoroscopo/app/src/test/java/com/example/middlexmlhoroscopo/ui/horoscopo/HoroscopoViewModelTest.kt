60ab72ad8d6abb86e97b00000de7dc9d71ff729a
Jamiro <devjamiro@gmail.com>
2024-10-15 22:43:04 -0300

    test(viewModel): add unit test for HoroscopoViewModel

--- middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoViewModelTest.kt
+++ middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/ui/horoscopo/HoroscopoViewModelTest.kt
@@ -1,5 +1,32 @@
 package com.example.middlexmlhoroscopo.ui.horoscopo
 
-import org.junit.Assert.*
+import com.example.middlexmlhoroscopo.data.providers.HoroscopoProvider
+import com.example.middlexmlhoroscopo.motherobject.HoroscopoMotherObject.horoscopoInfoList
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import junit.framework.TestCase.assertTrue
+import org.junit.Before
+import org.junit.Test
 
-class HoroscopoViewModelTest
+class HoroscopoViewModelTest {
+    @MockK
+    lateinit var horoscopoProvider: HoroscopoProvider
+
+    private lateinit var viewModel: HoroscopoViewModel
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+    }
+
+    @Test
+    fun `when ViewModel is created should get Horoscopo as a list`() {
+        /*Given*/
+        every { horoscopoProvider.getHoroscopo() } returns horoscopoInfoList
+        viewModel = HoroscopoViewModel(horoscopoProvider)
+
+        val horoscopes = viewModel.horoscopo.value
+        assertTrue(horoscopes.isNotEmpty())
+    }
+}
