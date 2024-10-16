cd3290cb73c29d8c8b36c86db20a39214d579d33
Jamiro <devjamiro@gmail.com>
2024-10-15 23:26:30 -0300

    test(ui): add MainActivityTest with Hilt and Espresso

--- middleXmlHoroscopo/app/src/androidTest/java/com/example/middlexmlhoroscopo/ui/home/MainActivityTest.kt
+++ middleXmlHoroscopo/app/src/androidTest/java/com/example/middlexmlhoroscopo/ui/home/MainActivityTest.kt
@@ -1,5 +1,37 @@
 package com.example.middlexmlhoroscopo.ui.home
 
-import org.junit.Assert.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.middlexmlhoroscopo.R
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-class MainActivityTest
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class MainActivityTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    var mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun when_main_activity_is_created_then_should_show_horoscope() {
+        onView(withId(R.id.luckFragment)).perform(click())
+    }
+
+ 
+}
