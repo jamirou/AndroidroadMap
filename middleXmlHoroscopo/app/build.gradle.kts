debd76c618337b4743e8983f9e3003f56ed01be7
Jamiro <devjamiro@gmail.com>
2024-10-15 21:35:32 -0300

    chore(dependencies): add kotlintest-runner-junit5

--- middleXmlHoroscopo/app/build.gradle.kts
+++ middleXmlHoroscopo/app/build.gradle.kts
@@ -74,15 +74,16 @@ dependencies {
     implementation(libs.androidx.camera.view)
     implementation(libs.androidx.camera.extensions)
 
-
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    /*Unit Tests*/
     testImplementation(libs.junit)
+    testImplementation(libs.kotlintest.runner.junit5)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
