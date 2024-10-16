161e0b925b57f687e5fb6b478586c65bb59ff3cc
Jamiro <devjamiro@gmail.com>
2024-10-15 23:36:15 -0300

    test(ui): enhance MainActivityTest with intent verification for horoscope detail

--- middleXmlHoroscopo/app/src/androidTest/java/com/example/middlexmlhoroscopo/ui/home/MainActivityTest.kt
+++ middleXmlHoroscopo/app/src/androidTest/java/com/example/middlexmlhoroscopo/ui/home/MainActivityTest.kt
@@ -1,13 +1,20 @@
 package com.example.middlexmlhoroscopo.ui.home
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.middlexmlhoroscopo.R
+import com.example.middlexmlhoroscopo.ui.detail.HoroscopoDetailActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,6 +33,11 @@ class MainActivityTest {
     @Before
     fun setUp() {
         hiltRule.inject()
+        Intents.init()
+    }
+    @After
+    fun tearDown() {
+        Intents.release()
     }
 
     @Test
@@ -33,5 +45,14 @@ class MainActivityTest {
         onView(withId(R.id.luckFragment)).perform(click())
     }
 
- 
+    @Test
+    fun when_horoscope_is_selected_should_show_detail() {
+        onView(withId(R.id.RecyclerViewHoroscopo)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        intended(hasComponent((HoroscopoDetailActivity::class.java.name)))
+    }
 }
