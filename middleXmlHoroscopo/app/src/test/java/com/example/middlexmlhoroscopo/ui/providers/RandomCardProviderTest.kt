f2e9c3d347628db14161e88965b5fcee4a2a76d3
Jamiro <devjamiro@gmail.com>
2024-10-15 22:06:51 -0300

    test(randomCardProvider): add unit test for getLucky method

---
+++ middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/ui/providers/RandomCardProviderTest.kt
@@ -0,0 +1,16 @@
+package com.example.middlexmlhoroscopo.ui.providers
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class RandomCardProviderTest {
+    @Test
+    fun `getRandomCard should return a random card`() {
+        /*Given*/
+        val randomCard = RandomCardProvider()
+        /*When*/
+        val card = randomCard.getLucky()
+        /*Then*/
+        assertNotNull(card)
+    }
+}
