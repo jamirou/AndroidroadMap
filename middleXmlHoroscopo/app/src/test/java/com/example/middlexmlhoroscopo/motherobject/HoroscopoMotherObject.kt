a482d3cdc96891f0b66bc4297e1a8907ef82a1e1
Jamiro <devjamiro@gmail.com>
2024-10-15 21:34:36 -0300

    test(motherobject): create HoroscopoMotherObject for reusable tests data

---
+++ middleXmlHoroscopo/app/src/test/java/com/example/middlexmlhoroscopo/motherobject/HoroscopoMotherObject.kt
@@ -0,0 +1,7 @@
+package com.example.middlexmlhoroscopo.motherobject
+
+import com.example.middlexmlhoroscopo.data.network.response.PredictionResponse
+
+object HoroscopoMotherObject {
+    val anyResponse = PredictionResponse("date", "Prediction", "taurus")
+}
