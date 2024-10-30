0f8df04e2f3dbb9fb9a7b1324cf9c0c31c69dc11
Jamiro <devjamiro@gmail.com>
2024-10-30 15:59:13 -0300

    feat(navigation): add NavigationWrapper to navigate between screens

--- FirebasePractice/app/src/main/java/com/example/firebasepractice/NavigationWrapper.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/NavigationWrapper.kt
@@ -1,2 +1,24 @@
 package com.example.firebasepractice
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.firebasepractice.presentation.initial.InitialScreen
+import com.example.firebasepractice.presentation.login.LoginScreen
+import com.example.firebasepractice.presentation.signup.SignUpScreen
+
+@Composable
+fun NavigationWrapper(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = "initial") {
+        composable("initial") {
+            InitialScreen()
+        }
+        composable("login") {
+            LoginScreen()
+        }
+        composable("signUp") {
+            SignUpScreen()
+        }
+    }
+}
