4f982d1703280bcade3088532f20fa89e5001cdf
Jamiro <devjamiro@gmail.com>
2024-10-30 13:37:39 -0300

    chore: add Crashlytics plugin for Firebase error reporting

--- FirebasePractice/app/build.gradle.kts
+++ FirebasePractice/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.googleServices)
+    alias(libs.plugins.crashlytics)
 }
 
 android {
