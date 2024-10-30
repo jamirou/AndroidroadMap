1f004b77f72fb2abbdd30be66fe9128d65897498
Jamiro <devjamiro@gmail.com>
2024-10-30 20:42:50 -0300

    show log for new logs

--- FirebasePractice/app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firebasepractice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,19 +11,35 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.firebasepractice.ui.theme.FirebasePracticeTheme
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : ComponentActivity() {
+
     private lateinit var navHostController: NavHostController
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         enableEdgeToEdge()
         setContent {
             navHostController = rememberNavController()
             FirebasePracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavigationWrapper(navHostController)
+                    NavigationWrapper(navHostController, auth)
                 }
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            /*navigate to home screen*/
+            Log.i("Jamiron", "Estoy logueado")
         }
     }
 }
