a07f7f0bcc8f3a7f97b7f1ef49ff3de13072bb6c
Jamiro <devjamiro@gmail.com>
2024-11-01 20:04:53 -0300

    feature(db): pass db parameter for storage

--- FirebasePractice/app/src/main/java/com/example/firebasepractice/MainActivity.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/MainActivity.kt
@@ -14,21 +14,25 @@ import com.example.firebasepractice.ui.theme.FirebasePracticeTheme
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.*
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var navHostController: NavHostController
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        db = com.google.firebase.ktx.Firebase.firestore
         enableEdgeToEdge()
         setContent {
             navHostController = rememberNavController()
             FirebasePracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    NavigationWrapper(navHostController, auth)
+                    NavigationWrapper(navHostController, auth, db)
                 }
             }
         }
