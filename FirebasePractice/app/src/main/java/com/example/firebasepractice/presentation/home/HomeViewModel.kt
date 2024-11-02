6be85eca2c8840c0db04b352f299ad62064d39df
Jamiro <devjamiro@gmail.com>
2024-11-02 14:48:02 -0300

    refactor: refactor data information to consume using coroutines insted in mainactivity

--- FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeViewModel.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeViewModel.kt
@@ -1,4 +1,48 @@
 package com.example.firebasepractice.presentation.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasepractice.presentation.model.Artist
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class HomeViewModel : ViewModel() {
+    private var db: FirebaseFirestore = com.google.firebase.ktx.Firebase.firestore
+    private val _artists = MutableStateFlow<List<Artist>>(emptyList())
+    val artists: StateFlow<List<Artist>> = _artists
+
+    init {
+        getArtists()
+    }
+
+    private fun getArtists() {
+        viewModelScope.launch {
+            val result: List<Artist> = withContext(Dispatchers.IO) {
+                getAllArtists()
+            }
+            _artists.value = result
+        }
+    }
+
+    private suspend fun getAllArtists(): List<Artist> {
+        return try {
+            db.collection("artists")
+                .get()
+                .await()
+                .documents
+                .mapNotNull { snapshot ->
+                    snapshot.toObject(Artist::class.java)
+                }
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+
 }
