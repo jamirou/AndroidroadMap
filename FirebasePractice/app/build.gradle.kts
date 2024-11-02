8bab2236313240b74bef991129d7dd65ac93de85
Jamiro <devjamiro@gmail.com>
2024-11-02 13:37:38 -0300

    feat(implementation): adding coil for image loading

--- FirebasePractice/app/build.gradle.kts
+++ FirebasePractice/app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation(libs.coil)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.navigation.compose)
