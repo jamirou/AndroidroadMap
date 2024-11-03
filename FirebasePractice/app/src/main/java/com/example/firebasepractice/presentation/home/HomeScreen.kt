825f36289cf390fa7cf1b5c1cac276a5d8483709
Jamiro <devjamiro@gmail.com>
2024-11-03 19:44:19 -0300

    feat(realtime): add functionality and view to display a realtime button showing the current state of the artist

--- FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeScreen.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.firebasepractice.presentation.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +17,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,6 +29,7 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.firebasepractice.presentation.model.Artist
 import com.example.firebasepractice.ui.theme.Black
+import com.example.firebasepractice.ui.theme.Purple80
 
 
 /*@Preview*/
@@ -32,6 +37,8 @@ import com.example.firebasepractice.ui.theme.Black
 fun HomeScreen(viewModel: HomeViewModel = HomeViewModel()) {
 
     val artists = viewModel.artists.collectAsState()
+    val player by viewModel.player.collectAsState()
+
     Column(
         Modifier
             .fillMaxSize()
@@ -50,6 +57,25 @@ fun HomeScreen(viewModel: HomeViewModel = HomeViewModel()) {
                 ArtistItem(it)
             }
         }
+        Spacer(modifier = Modifier.weight(1f))
+        if (player != null) {
+            val color = if (player?.play == true) Color.Green else Color.Red
+            Row(
+                modifier = Modifier
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .background(Purple80), verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(text = player?.artist?.name.orEmpty())
+                Spacer(modifier = Modifier.weight(1f))
+                Box(
+                    Modifier
+                        .size(20.dp)
+                        .background(color).clickable { viewModel.onPlaySelected() }
+                )
+            }
+        }
     }
 }
 
@@ -59,7 +85,9 @@ fun ArtistItem(artis: Artist) {
         AsyncImage(
             model = artis.image,
             contentDescription = "Artist image",
-            modifier = Modifier.size(70.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(70.dp)
+                .clip(CircleShape)
         )
         Spacer(modifier = Modifier.height(4.dp))
         Text(text = artis.name.orEmpty(), color = Color.White)
--- FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeViewModel.kt
+++ FirebasePractice/app/src/main/java/com/example/firebasepractice/presentation/home/HomeViewModel.kt
@@ -4,30 +4,58 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.firebasepractice.presentation.model.Artist
+import com.example.firebasepractice.presentation.model.Player
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class HomeViewModel : ViewModel() {
+    private val realtimeDatabase = Firebase.database
     private var db: FirebaseFirestore = com.google.firebase.ktx.Firebase.firestore
     private val _artists = MutableStateFlow<List<Artist>>(emptyList())
     val artists: StateFlow<List<Artist>> = _artists
+
+    private val _player = MutableStateFlow<Player?>(null)
+    val player: StateFlow<Player?> = _player
+
 
     init {
         /*repeat(20) {
         loadData()
         }*/
         getArtists()
+        getPlayer()
+    }
+
+    private fun getPlayer() {
+        viewModelScope.launch {
+            collectPlayer().collect {
+                val player = it.getValue(Player::class.java)
+                _player.value = player
+            }
+        }
     }
 
     private fun loadData() {
         val random = (1..10000).random()
-        val artist = Artist(name = "Artist $random", description = "Description $random", image = "https://picsum.photos/200")
+        val artist = Artist(
+            name = "Artist $random",
+            description = "Description $random",
+            image = "https://picsum.photos/200"
+        )
         db.collection("artists")
             .add(artist)
 
@@ -56,5 +84,35 @@ class HomeViewModel : ViewModel() {
         }
     }
 
+
+    private fun collectPlayer(): Flow<DataSnapshot> = callbackFlow {
+        val listener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                trySend(snapshot).isSuccess
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("Jamiron", "Error: ${error.message}")
+                close(error.toException())
+            }
+
+        }
+
+        val ref = realtimeDatabase.reference.child("player")
+        ref.addValueEventListener(listener)
+
+        awaitClose {
+            ref.removeEventListener(listener)
+        }
+
+    }
+
+    fun onPlaySelected() {
+        if (player.value != null) {
+            val currentPlayer = _player.value?.copy(play = !player.value?.play!!)
+            val ref = realtimeDatabase.reference.child("player")
+            ref.setValue(currentPlayer)
+        }
+    }
 
 }
